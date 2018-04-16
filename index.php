1aa28f2c1b12069feca999eb21c1edc8a56f9590
MoreThrust <peeraphat1997@gmail.com>
2018-04-17 00:58:32 +0700

    K'oo

--- index.php
+++ index.php
@@ -2,47 +2,23 @@
  
 $strAccessToken = "04jXv6we9MYpqRctFYw7mNbBUIU0Wb22RVFrmfSaJup0Ii+Uf3INLI5FzsSdP1uysuqnv/YvY300eOcXdgPygsQJ/QPsY1CTHe9QAoR2E14pw346tN2johPVIVUMO3CaBx/7W9TkKsXdTFRqL2+UJgdB04t89/1O/w1cDnyilFU=";
  
-$strUrl = "https://api.line.me/v2/bot/message/push";
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+ 
+$strUrl = "https://api.line.me/v2/bot/message/reply";
  
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-$arrPostData = array();
-$arrPostData['to'] = "U25ededce0fb6209d9efa4a85be630e3c";
-$arrPostData['messages'][0]['type'] = "postback";
-$arrPostData['messages'][0]['label'] = "Buy";
-$arrPostData['messages'][0]['data'] = "action=buy&itemid=111";
-$arrPostData['messages'][0]['text'] = "Buy";
-/*
-{  
-   "type":"postback",
-   "label":"Buy",
-   "data":"action=buy&itemid=111",
-   "text":"Buy"
+if($arrJson['events'][0]['message']['text'] == "K"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken']
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
 }
-
-{
-  "type": "template",
-  "altText": "this is a confirm template",
-  "template": {
-      "type": "confirm",
-      "text": "Are you sure?",
-      "actions": [
-          {
-            "type": "message",
-            "label": "Yes",
-            "text": "yes"
-          },
-          {
-            "type": "message",
-            "label": "No",
-            "text": "no"
-          }
-      ]
-  }
-}*/
-
+ 
+ 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
