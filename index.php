01713802d099c6bf32dcbce71cb188006ca2cc53
MoreThrust <peeraphat1997@gmail.com>
2018-04-18 18:12:09 +0700

    koo

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 
 include ('line-bot.php');
 
-$channelSecret = 'ee683a19a016dc7af6706b608f71d4c5
-';
+$channelSecret = 'ee683a19a016dc7af6706b608f71d4c5';
 $access_token  = '04jXv6we9MYpqRctFYw7mNbBUIU0Wb22RVFrmfSaJup0Ii+Uf3INLI5FzsSdP1uysuqnv/YvY300eOcXdgPygsQJ/QPsY1CTHe9QAoR2E14pw346tN2johPVIVUMO3CaBx/7W9TkKsXdTFRqL2+UJgdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
