97858bbde63b20297694061933e0c96f24eeff78
MoreThrust <peeraphat1997@gmail.com>
2018-04-19 09:29:07 +0700

    Koo

--- index.php
+++ index.php
@@ -1,23 +1,9 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart Office</title>
+</head>
+<body>
 
-include ('line-bot.php');
-
-$channelSecret = 'ee683a19a016dc7af6706b608f71d4c5';
-$access_token  = '04jXv6we9MYpqRctFYw7mNbBUIU0Wb22RVFrmfSaJup0Ii+Uf3INLI5FzsSdP1uysuqnv/YvY300eOcXdgPygsQJ/QPsY1CTHe9QAoR2E14pw346tN2johPVIVUMO3CaBx/7W9TkKsXdTFRqL2+UJgdB04t89/1O/w1cDnyilFU=';
-
-$bot = new BOT_API($channelSecret, $access_token);
-	
-if (!empty($bot->isEvents)) {
-		
-	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
-
-	if ($bot->isSuccess()) {
-		echo 'Succeeded!';
-		exit();
-	}
-
-	// Failed
-	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
-	exit();
-
-}
+</body>
+</html>
