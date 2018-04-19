d96d50c5ad1ae842bd91f0e9d963765f4b1733dd
MoreThrust <peeraphat1997@gmail.com>
2018-04-19 09:32:21 +0700

    Koo

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 <html>
 <head>
 	<title>Smart Office</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-
+<div class="container">
+<center>
+	<h1 class="display-4">Smart Office</h1>
+</center>
+</div>
 </body>
 </html>
