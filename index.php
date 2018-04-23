be63d4fd7cb1db9e0078a7812501e93db27128fc
MoreThrust <peeraphat1997@gmail.com>
2018-04-23 23:04:45 +0700

    Koo

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ require "vendor/autoload.php";
 $access_token = '04jXv6we9MYpqRctFYw7mNbBUIU0Wb22RVFrmfSaJup0Ii+Uf3INLI5FzsSdP1uysuqnv/YvY300eOcXdgPygsQJ/QPsY1CTHe9QAoR2E14pw346tN2johPVIVUMO3CaBx/7W9TkKsXdTFRqL2+UJgdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'ee683a19a016dc7af6706b608f71d4c5';
 $pushID = 'U25ededce0fb6209d9efa4a85be630e3c';
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
 if($st_front_door == "1") {
+  $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+  $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+  $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+  $response = $bot->pushMessage($pushID, $textMessageBuilder);
   echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 }
 
