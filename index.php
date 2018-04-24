e50ae2e3ba03285ef9800a8e783ec0e2b63cfedf
MoreThrust <peeraphat1997@gmail.com>
2018-04-24 11:45:36 +0700

    Koo

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ elseif($message->{"text"} == 'แสงสว่างทางเดิน') {
     $curl = curl_init();
     curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/set/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/lamp_walkway/1',
+    CURLOPT_URL => 'https://api.anto.io/channel/set/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/lamp_walkway/0',
     CURLOPT_USERAGENT => 'Codular Sample cURL Request'
     ));
     $resp = curl_exec($curl);curl_close($curl);
