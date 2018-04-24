6764da3f17f7170696d1de8df9ad36f744259b30
MoreThrust <peeraphat1997@gmail.com>
2018-04-24 16:21:52 +0700

    Koo

--- index.php
+++ index.php
@@ -282,7 +282,7 @@ if ($message->{"text"} == 'แอร์') {
             'actions' => [
                 [
                     'type' => 'message',
-                    'label' => $st_lamp_rt,
+                    'label' => $temp_air_rt,
                     'text' => 'แอร์ห้องรับแขก'
                 ],
                 [
