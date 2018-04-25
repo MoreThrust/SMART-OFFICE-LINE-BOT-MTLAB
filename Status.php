65f3f3fd6a7f83c8964cc87bd0ea4c5ead98b7d3
MoreThrust <peeraphat1997@gmail.com>
2018-04-25 14:57:35 +0700

    Koo

--- Status.php
+++ Status.php
@@ -11,9 +11,9 @@ $resp = curl_exec($curl);
 curl_close($curl);
 $val = explode('"', $resp);
 if($val[7] == "1"){
-    $st_lamp_ws = "ไฟห้องทำงาน: เปิดอยู่ 􀔃􀇣blue circle􏿿";
+    $st_lamp_ws = "ไฟห้องทำงาน: เปิดอยู่";
 }elseif($val[7] == "0"){
-    $st_lamp_ws = "ไฟห้องทำงาน: ปิดอยู่ 􀔃􀇢red circle􏿿";
+    $st_lamp_ws = "ไฟห้องทำงาน: ปิดอยู่";
 }
 ?>
 
@@ -28,9 +28,9 @@ $resp = curl_exec($curl);
 curl_close($curl);
 $val = explode('"', $resp);
 if($val[7] == "1"){
-    $st_lamp_mt = "ไฟห้องประชุม: เปิดอยู่ 􀔃􀇣blue circle􏿿";
+    $st_lamp_mt = "ไฟห้องประชุม: เปิดอยู่";
 }elseif($val[7] == "0"){
-    $st_lamp_mt = "ไฟห้องประชุม: ปิดอยู่ 􀔃􀇢red circle􏿿";
+    $st_lamp_mt = "ไฟห้องประชุม: ปิดอยู่";
 } 
 ?>
 
@@ -45,9 +45,9 @@ $resp = curl_exec($curl);
 curl_close($curl);
 $val = explode('"', $resp);
 if($val[7] == "1"){
-    $st_lamp_rt = "ไฟห้องรับแขก: เปิดอยู่ 􀔃􀇣blue circle􏿿";
+    $st_lamp_rt = "ไฟห้องรับแขก: เปิดอยู่";
 }elseif($val[7] == "0"){
-    $st_lamp_rt = "ไฟห้องรับแขก: ปิดอยู่ 􀔃􀇢red circle􏿿";
+    $st_lamp_rt = "ไฟห้องรับแขก: ปิดอยู่";
 }   
 ?>
 
@@ -66,9 +66,9 @@ $resp = curl_exec($curl);
 curl_close($curl);
 $val = explode('"', $resp);
 if($val[7] == "1"){
-    $st_air_ws = "แอร์ห้องทำงาน: เปิดอยู่ 􀔃􀇣blue circle􏿿";
+    $st_air_ws = "แอร์ห้องทำงาน: เปิดอยู่";
 }elseif($val[7] == "0"){
-    $st_air_ws = "แอร์ห้องทำงาน: ปิดอยู่ 􀔃􀇢red circle􏿿";
+    $st_air_ws = "แอร์ห้องทำงาน: ปิดอยู่";
 }
 ?>
 
@@ -83,9 +83,9 @@ $resp = curl_exec($curl);
 curl_close($curl);
 $val = explode('"', $resp);
 if($val[7] == "1"){
-    $st_air_mt = "แอร์ห้องประชุม: เปิดอยู่ 􀔃􀇣blue circle􏿿";
+    $st_air_mt = "แอร์ห้องประชุม: เปิดอยู่";
 }elseif($val[7] == "0"){
-    $st_air_mt = "แอร์ห้องประชุม: ปิดอยู่ 􀔃􀇢red circle􏿿";
+    $st_air_mt = "แอร์ห้องประชุม: ปิดอยู่";
 }
 ?>
 
@@ -100,9 +100,9 @@ $resp = curl_exec($curl);
 curl_close($curl);
 $val = explode('"', $resp);
 if($val[7] == "1"){
-    $st_air_rt = "แอร์ห้องรับแขก: เปิดอยู่ 􀔃􀇣blue circle􏿿";
+    $st_air_rt = "แอร์ห้องรับแขก: เปิดอยู่";
 }elseif($val[7] == "0"){
-    $st_air_rt = "แอร์ห้องรับแขก: ปิดอยู่ 􀔃􀇢red circle􏿿";
+    $st_air_rt = "แอร์ห้องรับแขก: ปิดอยู่";
 }   
 ?>
 
@@ -164,9 +164,9 @@ $resp = curl_exec($curl);
 curl_close($curl);
 $val = explode('"', $resp);
 if($val[7] == "1"){
-    $st_door_et = "ทางเข้า: ล็อกอยู่ 􀔃􀆑lock􏿿";
+    $st_door_et = "ทางเข้า: ล็อกอยู่";
 }elseif($val[7] == "0"){
-    $st_door_et = "ทางเข้า: ไม่มีการล็อก 􀔃􀆒opened lock􏿿";
+    $st_door_et = "ทางเข้า: ไม่มีการล็อก";
 }
 ?>
 
@@ -181,9 +181,9 @@ $resp = curl_exec($curl);
 curl_close($curl);
 $val = explode('"', $resp);
 if($val[7] == "1"){
-    $st_door_ws = "ห้องทำงาน: ล็อกอยู่ 􀔃􀆑lock􏿿";
+    $st_door_ws = "ห้องทำงาน: ล็อกอยู่";
 }elseif($val[7] == "0"){
-    $st_door_ws = "ห้องทำงาน: ไม่มีการล็อก 􀔃􀆒opened lock􏿿";
+    $st_door_ws = "ห้องทำงาน: ไม่มีการล็อก";
 } 
 ?>
 
@@ -191,16 +191,16 @@ if($val[7] == "1"){
 $curl = curl_init();
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/door_meeting_room',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/door_meeting_room'
+    /*CURLOPT_USERAGENT => 'Codular Sample cURL Request'*/
 ));
 $resp = curl_exec($curl);
 curl_close($curl);
 $val = explode('"', $resp);
 if($val[7] == "1"){
-    $st_door_mt = "ห้องประชุม: ล็อกอยู่ 􀔃􀆑lock􏿿";
+    $st_door_mt = "ห้องประชุม: ล็อกอยู่";
 }elseif($val[7] == "0"){
-    $st_door_mt = "ห้องประชุม: ไม่มีการล็อก 􀔃􀆒opened lock􏿿";
+    $st_door_mt = "ห้องประชุม: ไม่มีการล็อก";
 }   
 ?>
 
