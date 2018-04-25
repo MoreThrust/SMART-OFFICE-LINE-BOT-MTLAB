96c9d8e1c20cde46c59d09d63f4125ceafeebc00
MoreThrust <peeraphat1997@gmail.com>
2018-04-25 22:48:17 +0700

    Koo

--- index.php
+++ index.php
@@ -790,7 +790,7 @@ elseif($message->{"text"} == 'ประตูทั้งหมด') {
 if ($message->{"text"} == 'สถานะ') {
     $messageData = [
         "type" => "text",
-        "text" => "􀔃􀄒novice􏿿 สถานะโดยรวม\n􁤁􀆅energy saving bulb􏿿 แสงสว่าง\n- ".$st_lamp_rt.$st_lamp_rt2."\n- ".$st_lamp_ws."\n- ".$st_lamp_mt."\n\nเครื่องทำความเย็น\n- ".$st_air_rt.$st_air_rt2."\n- ".$st_air_ws."\n".$st_air_mt."\n\n􀌂􀇨snowy􏿿 อุณหภูมิห้อง\n- ".$temp_air_rt."\n- ".$temp_air_ws."\n- ".$temp_air_mt."\n\n􀐂􀇚กุญแจ􏿿 ระบบความปลอดภัย\n- ".$st_door_et."\n- ".$st_door_ws."\n- ".$st_door_mt."\n"
+        "text" => "􀔃􀄒novice􏿿 สถานะโดยรวม\n􁤁􀆅energy saving bulb􏿿 แสงสว่าง\n- ".$st_lamp_rt.$st_lamp_rt2."\n- ".$st_lamp_ws.$st_lamp_ws2."\n- ".$st_lamp_mt.$st_lamp_mt2."\n\nเครื่องทำความเย็น\n- ".$st_air_rt.$st_air_rt2."\n- ".$st_air_ws.$st_air_ws2."\n".$st_air_mt.$st_air_mt2."\n\n􀌂􀇨snowy􏿿 อุณหภูมิห้อง\n- ".$temp_air_rt.$temp_air_rt2."\n- ".$temp_air_ws.$temp_air_ws2."\n- ".$temp_air_mt.$temp_air_mt2."\n\n􀐂􀇚กุญแจ􏿿 ระบบความปลอดภัย\n- ".$st_door_et.$st_door_et2."\n- ".$st_door_ws.$st_door_ws2."\n- ".$st_door_mt.$st_door_mt2."\n"
     ];
 } 
 
